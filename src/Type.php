df68cd7dae0edccd9939887b64e07b67bb909d16
Adam Ross Russell and Jordan Meier <jbirdmeier@gmail.com>
2016-02-23 14:32:40 -0800

    add specs and pass tests for save, getAll and deleteAll methods in Type class

--- src/Type.php
+++ src/Type.php
@@ -19,5 +19,29 @@
         {
             return $this->animal_type;
         }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO type (animal_type) VALUES ('{$this->getAnimalType()}')");
+            $this->id= $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_type = $GLOBALS['DB']->query("SELECT * FROM type;");
+            $output_types = array();
+            foreach($returned_type as $type) {
+                $animal_type = $type['animal_type'];
+                $id = $type['id'];
+                $new_type = new Type($animal_type, $id);
+                array_push($output_types, $new_type);
+            }
+            return $output_types;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM type;");
+        }
     }
  ?>
---
+++ tests/TypeTest.php
@@ -0,0 +1,75 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Type.php";
+    require_once "src/Animal.php";
+
+    $server = 'mysql:host=localhost;dbname=animal_shelter_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class TypeTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+          Type::deleteAll();
+        //   Animal::deleteAll();
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $animal_type = "Dog";
+            $test_Type = new Type($animal_type);
+            $test_Type->save();
+
+            //Act
+            $result = Type::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Type], $result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $animal_type = "Dog";
+            $animal_type2 = "Cat";
+            $test_Type = new Type($animal_type);
+            $test_Type->save();
+            $test_Type2 = new Type($animal_type2);
+            $test_Type2->save();
+
+            //Act
+            $result = Type::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Type, $test_Type2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $animal_type = "Dog";
+            $animal_type2 = "Cat";
+            $test_Type = new Type($animal_type);
+            $test_Type->save();
+            $test_Type2 = new Type($animal_type2);
+            $test_Type2->save();
+
+            //Act
+            Type::deleteAll();
+            $result = Type::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+    }
+
+?>
