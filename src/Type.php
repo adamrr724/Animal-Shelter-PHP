115c81d09b7c16f46525f2c6139c3efacd9acf14
Adam Ross Russell and Jordan Meier <jbirdmeier@gmail.com>
2016-02-23 16:14:13 -0800

    add a spec and pass test for getAnimals method in Type class

--- src/Type.php
+++ src/Type.php
@@ -43,5 +43,23 @@
         {
           $GLOBALS['DB']->exec("DELETE FROM type;");
         }
+
+        function getAnimals()
+        {
+            $animals = Array();
+            $returned_animals = $GLOBALS['DB']->query("SELECT * FROM animals WHERE animal_type_id = {$this->getId()};");
+            foreach($returned_animals as $animal) {
+                $name = $animal['name'];
+                $gender = $animal['gender'];
+                $age = $animal['age'];
+                $admittance_date = $animal['date_of_admittance'];
+                $breed = $animal['breed'];
+                $id = $animal['id'];
+                $type_id = $animal['animal_type_id'];
+                $new_animal = new Animal($name, $id, $age, $admittance_date, $gender, $breed, $type_id);
+                array_push($animals, $new_animal);
+            }
+            return $animals;
+        }
     }
  ?>
--- tests/TypeTest.php
+++ tests/TypeTest.php
@@ -19,7 +19,7 @@
         protected function tearDown()
         {
           Type::deleteAll();
-        //   Animal::deleteAll();
+          Animal::deleteAll();
         }
 
         function test_save()
@@ -69,6 +69,41 @@
 
             //Assert
             $this->assertEquals([], $result);
+        }
+
+        function test_getAnimals()
+        {
+            //Arrange
+            $animal_type = "Dog";
+            $id = null;
+            $test_type = new Type($animal_type, $id);
+            $test_type->save();
+
+            $test_type_id = $test_type->getId();
+
+            $name = "Fluffy";
+            $gender = 'female';
+            $age = 12;
+            $admittance_date = '2016-03-23';
+            $breed = 'bulldog';
+            $type_id = $test_type->getId();
+            $test_animal = new Animal($name, $id, $age, $admittance_date, $gender, $breed, $type_id);
+            $test_animal->save();
+
+            $name2 = "Dingo";
+            $gender2 = 'male';
+            $age2 = 1;
+            $admittance_date2 = '2016-03-22';
+            $breed2 = 'greyhound';
+            $type_id2 = $test_type->getId();
+            $test_animal2 = new Animal($name2, $id, $age2, $admittance_date2, $gender2, $breed2, $type_id2);
+            $test_animal2->save();
+
+            //Act
+            $result = $test_type->getAnimals();
+
+            //Assert
+            $this->assertEquals([$test_animal, $test_animal2], $result);
         }
     }
 
